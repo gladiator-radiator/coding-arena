f7c6dcc4c12f44e95befaeab4eb0739a7d66730b
David Busek <admin@4sure.com>
2026-02-25 20:10:21 +0100

    Initial commit: Spring Boot skeleton

---
+++ src/main/java/cz/arena/coding_arena/CodingArenaApplication.java
@@ -0,0 +1,13 @@
+package cz.arena.coding_arena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingArenaApplication.class, args);
+	}
+
+}
