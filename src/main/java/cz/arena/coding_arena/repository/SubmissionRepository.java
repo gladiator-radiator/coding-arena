6f95d48aa5137be05202126b4adb52ef819d0dc3
David Busek <davidbusek05@gmail.com>
2026-04-13 23:11:02 +0200

    fix: restore missing repository and dto files

--- src/main/java/cz/arena/coding_arena/repository/SubmissionRepository.java
+++ src/main/java/cz/arena/coding_arena/repository/SubmissionRepository.java
@@ -1,7 +1,9 @@
 package cz.arena.coding_arena.repository;
+
 import cz.arena.coding_arena.model.Submission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SubmissionRepository extends JpaRepository<Submission, Long> {}
+public interface SubmissionRepository extends JpaRepository<Submission, Long> {
+}
--- src/main/java/cz/arena/coding_arena/repository/TestCaseRepository.java
+++ src/main/java/cz/arena/coding_arena/repository/TestCaseRepository.java
@@ -1,7 +1,11 @@
 package cz.arena.coding_arena.repository;
+
 import cz.arena.coding_arena.model.TestCase;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
-public interface TestCaseRepository extends JpaRepository<TestCase, Long> {}
+public interface TestCaseRepository extends JpaRepository<TestCase, Long> {
+    List<TestCase> findByTaskId(Long taskId);
+}
